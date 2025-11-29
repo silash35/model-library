aaa714efc25de35cb5b1085ffb0c98be4e678e0a
Silas Henrique <silash35@gmail.com>
2025-11-29 17:50:25 -0300

    Add experiment: Linear vs Nonlinear Pendulum

---
+++ experiments/linear-vs-nonlinear-pendulum/README.md
@@ -0,0 +1,21 @@
+# Linear vs Nonlinear Pendulum
+
+This experiment compares the **linear** and **nonlinear** models of the simple pendulum.
+Both describe the same system, but we want to test how well the linear model approximates the true dynamics.
+
+## 📎 Related Models
+
+- [**Simple Pendulum**](/models/mechanical/simple-pendulum/README.md)
+- [**Linear Simple Pendulum**](/models/mechanical/linear-simple-pendulum/README.md)
+
+## 🧪 Methodology
+
+We simulate both models under identical conditions, varying only the **initial angular displacement**. Three initial angles are tested: 15°, 30°, and 60°.
+
+For each case, the models are integrated over the same time interval using the same numerical solver, allowing a direct comparison of their predicted trajectories.
+
+## 📊 Results and Conclusions
+
+<img src="results/scipy.png" alt="Pendulum Comparison Plot (Scipy)"/>
+
+As seen in the simulation results, the linear model matches the nonlinear behavior well for **small angles**, but as the initial angle increases, the trajectories **diverge significantly**, and the linear approximation becomes invalid.
---
+++ experiments/linear-vs-nonlinear-pendulum/sim_scipy.py
@@ -0,0 +1,69 @@
+import os
+from typing import Final
+
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.constants import g as gravity
+from scipy.integrate import solve_ivp
+
+# --- Define Models ---
+g: Final[float] = gravity  # Gravitational acceleration [m/s²]
+
+L: Final[float] = 1.0  # Pendulum length [m]
+
+
+def diff_equations(t: float, y: np.ndarray):
+    theta = y[0]  # Angle for nonlinear pendulum [rad]
+    omega = y[1]  # Angular velocity for nonlinear pendulum [rad/s]
+
+    linear_theta = y[2]  # Angle for linear pendulum [rad]
+    linear_omega = y[3]  # Angular velocity for linear pendulum [rad/s]
+
+    # Nonlinear model
+    dtheta_dt = omega
+    domega_dt = -(g / L) * np.sin(theta)
+
+    # Linear model
+    dlinear_theta_dt = linear_omega
+    dlinear_omega_dt = -(g / L) * linear_theta
+    return [dtheta_dt, domega_dt, dlinear_theta_dt, dlinear_omega_dt]
+
+
+# --- Make simulations ---
+t = np.linspace(0, 10, 1000)  # Simulation time [s]
+test_angles = np.deg2rad(np.array([15, 30, 60]))
+solutions = np.zeros((test_angles.size, 2, t.size))
+
+print(solutions.shape)
+
+for i in range(test_angles.size):
+    y0 = [test_angles[i], 0.0, test_angles[i], 0.0]
+    sol = solve_ivp(diff_equations, [t[0], t[-1]], y0, t_eval=t)
+    solutions[i, 0] = sol.y[0]
+    solutions[i, 1] = sol.y[2]
+
+
+# --- Plot results ---
+# Convert angle to degrees
+solutions = np.rad2deg(solutions)
+
+fig, axs = plt.subplots(
+    test_angles.size, 1, figsize=(8, 6), sharex=True, constrained_layout=True
+)
+fig.suptitle("Linear vs Nonlinear Pendulum")
+
+for i in range(test_angles.size):
+    axs[i].set_title(f"Initial Angle = {np.rad2deg(test_angles[i]):.0f}°")
+    axs[i].plot(t, solutions[i, 0], label="Nonlinear")
+    axs[i].plot(t, solutions[i, 1], label="Linear")
+    axs[i].set_ylabel("$\\theta(t)$ / deg")
+    axs[i].grid(True)
+    axs[i].legend(loc="upper right")
+axs[-1].set_xlabel("Time / s")
+
+# Save plot to file
+script_dir = os.path.dirname(os.path.abspath(__file__))
+os.makedirs(os.path.join(script_dir, "results"), exist_ok=True)
+save_path = os.path.join(script_dir, "results", "scipy.png")
+plt.savefig(save_path)
+print(f"Plot saved to {save_path}")
